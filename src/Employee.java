8c0218903e908f38d4438153174b7f02235fe45e
yuri-1984 <yuri.hiraoka@rakus-partners.co.jp>
2019-10-24 13:47:24 +0900

    従業員のクラスを作成します

---
+++ src/Employee.java
@@ -0,0 +1,19 @@
+
+public class Employee {
+	String name;
+	int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
